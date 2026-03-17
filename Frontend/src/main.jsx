754e187c7f35a6a4744ce4d895d5eab202a94f40
Divyaraj Purohit <divyarajpurohit000@gmail.com>
2026-03-17 21:00:14 +0530

    little changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,9 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import Wrapper from './context/Wrapper.jsx'
-import {dotenv} from 'dotenv'
-
-dotenv.config()
 
 createRoot(document.getElementById('root')).render(
   <Wrapper>
